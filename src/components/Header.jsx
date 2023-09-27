671767f07606be9d38af6a68ac805d2dca0dd98c
Minjoo <m0522inju@gmail.com>
2023-09-27 19:54:21 +0900

    design : header CSS styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,21 +7,23 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header>
-      <Link to='/'>
+    <header className='w-full flex justify-between p-4 bg-white mb-4'>
+      <Link to='/' className='flex items-center gap-1 text-2xl text-brand'>
         <BsFillHeartFill />
-        <h1>Shoppy</h1>
+        <h1 className='font-bold'>Shoppy</h1>
       </Link>
-      <Link to='/products'>
-        <button>Products</button>
-      </Link>
-      <Link to='/carts'>
-        <BsCart />
-      </Link>
-      <Link to='/products/new'>
-        <BsPencilFill />
-      </Link>
-      <button>Login</button>
+      <div className='flex items-center gap-4 text-lg font-semibold'>
+        <Link to='/products'>
+          <button>Products</button>
+        </Link>
+        <Link className='text-xl' to='/carts'>
+          <BsCart />
+        </Link>
+        <Link to='/products/new'>
+          <BsPencilFill />
+        </Link>
+        <button className='px-4 py-1.5 bg-brand text-white rounded'>Login</button>
+      </div>
     </header>
   );
 }
