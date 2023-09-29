9d8540753ce475191843249bd5db8f9b574ae3d0
Minjoo <m0522inju@gmail.com>
2023-09-30 07:39:23 +0900

    design : NewProducts styling

--- src/pages/NewProducts.jsx
+++ src/pages/NewProducts.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 export default function NewProducts() {
   const [imgFile, setImgFile] = useState('');
   const [itemName, setItemName] = useState('');
-  const [price, setPrice] = useState(0);
+  const [price, setPrice] = useState('');
   const [category, setCategory] = useState('');
   const [info, setInfo] = useState('');
   const [options, setOptions] = useState('');
@@ -37,35 +37,41 @@ export default function NewProducts() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type='file' name='file' id='file' value={imgFile} onChange={handleFileChange} />
-      <input
-        type='text'
-        name='itemName'
-        id='itemName'
-        value={itemName}
-        placeholder='제품명'
-        onChange={handleItemNameChange}
-      />
-      <input type='number' name='price' id='price' value={price} placeholder='가격' onChange={handlePriceChange} />
-      <input
-        type='text'
-        name='category'
-        id='category'
-        value={category}
-        placeholder='카테고리'
-        onChange={handleCategoryChange}
-      />
-      <input type='text' name='info' id='info' value={info} placeholder='제품 설명' onChange={handleInfoChange} />
-      <input
-        type='text'
-        name='options'
-        id='options'
-        priceholder='옵션들(콤마(,)로 구분)'
-        value={options}
-        onChange={handleOptionsChange}
-      />
-      <button type='submit'>제품 등록</button>
-    </form>
+    <main className='w-full flex flex-col items-center'>
+      <h2 className='font-bold text-2xl mb-8'>신제품 등록</h2>
+      <form className='flex flex-col gap-4 w-6/12 drop-shadow-md' onSubmit={handleSubmit}>
+        {/* TODO: img 업로드 컴포넌트 분리 */}
+        <input type='file' name='file' id='file' value={imgFile} onChange={handleFileChange} />
+        <input
+          type='text'
+          name='itemName'
+          id='itemName'
+          value={itemName}
+          placeholder='제품명'
+          onChange={handleItemNameChange}
+        />
+        <input type='text' name='price' id='price' value={price} placeholder='가격' onChange={handlePriceChange} />
+        <input
+          type='text'
+          name='category'
+          id='category'
+          value={category}
+          placeholder='카테고리'
+          onChange={handleCategoryChange}
+        />
+        <input type='text' name='info' id='info' value={info} placeholder='제품 설명' onChange={handleInfoChange} />
+        <input
+          type='text'
+          name='options'
+          id='options'
+          placeholder='옵션들(콤마(,)로 구분)'
+          value={options}
+          onChange={handleOptionsChange}
+        />
+        <button type='submit' className='px-4 py-1.5 bg-brand text-white rounded w-fit m-auto mt-4 text-xl font-bold'>
+          제품 등록
+        </button>
+      </form>
+    </main>
   );
 }
